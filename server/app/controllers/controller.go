07de5608f91ed3e220aebd839155f6bea4b6747a
Lalit <lalitkumarsingh3716@gmail.com>
2022-10-06 20:24:01 +0530

    feat: Added Github Workflow for Server Deployment

--- server/app/controllers/controller.go
+++ server/app/controllers/controller.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
-	"github.com/Lalit3716/ideabook_server/utils"
 	"net/http"
+
+	"github.com/Lalit3716/ideabook_server/utils"
 )
 
 func GetHello(w http.ResponseWriter, _ *http.Request) {
-	utils.ResponseJSON(w, http.StatusOK, "Hello World")
+	utils.ResponseJSON(w, http.StatusOK, "Hello World!!")
 }
