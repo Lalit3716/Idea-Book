afeb4277d232a6747fe29bf0ea16f1501dc9ad48
Lalit <lalitkumarsingh3716@gmail.com>
2022-10-08 15:51:11 +0530

    fix: Using env variables

--- server/app/config/db_config.go
+++ server/app/config/db_config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+	"strconv"
+)
+
 type DBConfig struct {
 	Dialect  string
 	Host     string
@@ -11,13 +18,26 @@ type DBConfig struct {
 }
 
 func GetDBConfig() *DBConfig {
+	err := godotenv.Load(".env.dev")
+
+	if err != nil {
+		fmt.Println("Error loading env file")
+	}
+
+	// Get the value of the PORT environment variable
+	port, e := strconv.ParseInt(os.Getenv("DB_PORT"), 10, 16)
+
+	if e != nil {
+		fmt.Println("Error parsing DB_PORT in env file")
+	}
+
 	return &DBConfig{
-		Dialect:  "mysql",
-		Host:     "localhost",
-		Port:     3307,
-		Username: "root",
-		Password: "root",
-		Dbname:   "ideabook",
+		Dialect:  os.Getenv("DB_DIALECT"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     port,
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Dbname:   os.Getenv("DB_NAME"),
 		Charset:  "utf8mb4",
 	}
 }
