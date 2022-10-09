171bc179596a7ab8136fe1dedb8925c204fdcafc
Lalit <lalitkumarsingh3716@gmail.com>
2022-10-09 17:14:37 +0530

    feat: Added a no content component

--- android/app/src/main/java/com/example/idea_book/presentation/ideas/IdeasScreen.kt
+++ android/app/src/main/java/com/example/idea_book/presentation/ideas/IdeasScreen.kt
@@ -1,18 +1,12 @@
 package com.example.idea_book.presentation.ideas
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.idea_book.R
 import com.example.idea_book.presentation.common.layout.Layout
 import com.example.idea_book.presentation.destinations.CreateIdeaScreenDestination
 import com.example.idea_book.presentation.ideas.components.NoContent
