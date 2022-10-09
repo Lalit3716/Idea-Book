332eb37cc40807d96114bfd076995ae3662b432e
Lalit <lalitkumarsingh3716@gmail.com>
2022-10-09 17:14:19 +0530

    feat: Added a no content component

--- android/app/src/main/java/com/example/idea_book/presentation/ideas/IdeasScreen.kt
+++ android/app/src/main/java/com/example/idea_book/presentation/ideas/IdeasScreen.kt
@@ -1,18 +1,21 @@
 package com.example.idea_book.presentation.ideas
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.idea_book.R
 import com.example.idea_book.presentation.common.layout.Layout
 import com.example.idea_book.presentation.destinations.CreateIdeaScreenDestination
+import com.example.idea_book.presentation.ideas.components.NoContent
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -35,17 +38,7 @@ fun IdeasScreen(
             color = MaterialTheme.colors.background,
             contentColor = MaterialTheme.colors.onBackground
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "Ideas Screen",
-                    style = MaterialTheme.typography.h5,
-                    textAlign = TextAlign.Center
-                )
-            }
+            NoContent()
         }
     }
 }
